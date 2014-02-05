ff31dde313094718395f14d7a483ba60594bd545
sampoz <sampsa.laapotti@iki.fi>
2014-02-05 17:12:44 +0200

    User registration page

--- django/neverlate/urls.py
+++ django/neverlate/urls.py
@@ -9,5 +9,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'neverlate.views.frontpage', )
+    url(r'^$', 'neverlate.views.frontpage', ),
+    url(r'^register/$', 'neverlate.views.register', name="Neverlate Registration"),
 )
--- django/neverlate/views.py
+++ django/neverlate/views.py
@@ -1,5 +1,17 @@
-
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response, render
 
 def frontpage(request):
     return render_to_response('base.html', {'user': request.user})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "register.html", {'form': form})
