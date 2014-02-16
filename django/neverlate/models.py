097e0fd597a3ac90877037907cfbcd1a83028421
Martin Yrjölä <martin.yrjola@gmail.com>
2014-02-16 20:07:55 +0200

    Fix __str__ for ICalURL

--- django/neverlate/models.py
+++ django/neverlate/models.py
@@ -1,4 +1,3 @@
-
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
@@ -19,7 +18,7 @@ class ICalURL(models.Model):
     url = models.CharField(max_length=400)
 
     def __str__(self):
-        return "%s's profile" % self.user
+        return "%s: %s" % (self.name, self.url)
 
 
 def create_user_profile(sender, instance, created, **kwargs):
@@ -28,4 +27,3 @@ def create_user_profile(sender, instance, created, **kwargs):
 
 
 post_save.connect(create_user_profile, sender=User)
-
