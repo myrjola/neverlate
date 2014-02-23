f075816228967cf374e8f4bb8042dcd9e558c4d3
Samuli Hynönen <samuli.hynonen@iki.fi>
2014-02-23 15:07:29 +0200

    a separate function call for parsing each route (leg)

--- django/static/js/Neverlate.js
+++ django/static/js/Neverlate.js
@@ -20,38 +20,42 @@ Neverlate.loadRoutes = function() {
         console.log("loading data fron routeplanner");
         var url= "/routeplanner";
         $.getJSON(url, function(result){
-            Neverlate.parseRoute(result);
+            Neverlate.parseAllRoutes(result);
         });
-
     });
 }
-Neverlate.parseRoute = function(data){
+
+Neverlate.parseAllRoutes = function(data){
     console.log(JSON.parse(data));
-    //TODO what to parse from reittiopas?
     var toJson = JSON.parse(data);
     var source = Neverlate.templates.route_front;
     var template = Handlebars.compile(source);
     var html = template(toJson[0][0]);
-    var coordinates = []; // TODO: put route data for maps here (1 entry for each leg)
     $("#content-placeholder").html(html);
-    Neverlate.loadMaps(coordinates);
-}
 
-
-Neverlate.loadMaps = function(coordinates){ //TODO give coordinates from parsed data
     var s = document.createElement("script");
     s.type = "text/javascript";
-    s.src  = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAoHieetxNcdqJ4PDij87fi2KH8tOhMK2Y&sensor=true&callback=gmap_draw";
+    s.src  = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAoHieetxNcdqJ4PDij87fi2KH8tOhMK2Y&sensor=true&callback=gmap_loaded";
     $("head").append(s);
-    window.gmap_draw = function(){
+    window.gmap_loaded = function(){
         $(".map-canvas").each(function(index) {
-            // TODO: use coordinates[index] to display to route on the map and center it
-            console.log("drawed a map");
-            var mapOptions = {
-                center: new google.maps.LatLng(-34.397, 150.644),
-                zoom: 8
-            };
-            var map = new google.maps.Map($(this)[0], mapOptions);
+            Neverlate.parseRoute($(this)[0], toJson[0][0][index]);
         });
     }
+}
+
+Neverlate.parseRoute = function(map_canvas, leg_data){
+    //TODO what to parse from reittiopas?
+    var route_data = null; // TODO: parse route_data from leg_data
+    Neverlate.loadMap(map_canvas, route_data);
+}
+
+Neverlate.loadMap = function(map_canvas, route_data){
+    console.log("drawed a map");
+    var mapOptions = {
+        center: new google.maps.LatLng(-34.397, 150.644),
+        zoom: 8
+    };
+    var map = new google.maps.Map(map_canvas, mapOptions);
+    // TODO: draw the route using route_data
 }
