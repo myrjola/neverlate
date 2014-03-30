cb7425d0ccd3f194d21fa76bafd38dfbc8a94230
myrjola <martin.yrjola@gmail.com>
2014-03-30 14:28:06 +0300

    Add LocationAlias admin stuff

--- django/neverlate/admin.py
+++ django/neverlate/admin.py
@@ -1,14 +1,18 @@
 
 from django.contrib import admin
-from models import UserProfile, ICalURL, TooltipMessage
+from models import UserProfile, ICalURL, TooltipMessage, LocationAlias
 
 
 class ICalURLInline(admin.TabularInline):
     model = ICalURL
 
 
+class LocationAliasInline(admin.TabularInline):
+    model = LocationAlias
+
+
 class UserProfileAdmin(admin.ModelAdmin):
-    inlines = [ICalURLInline]
+    inlines = [ICalURLInline, LocationAliasInline]
 
 
 admin.site.register(UserProfile, UserProfileAdmin)
